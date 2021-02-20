2ec5c8c0ca546385a960ca976827a144c3662aac
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-02-20 18:52:18 +0900

    bug fixed

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,17 +1,19 @@
 var main = {
-    init : fuction () {
+    init : function () {
         var _this = this;
         $('#btn-save').on('click', function () {
             _this.save();
-        })
+        });
+
         $('#btn-update').on('click', function () {
             _this.update();
-        })
+        });
+
         $('#btn-delete').on('click', function () {
             _this.delete();
         });
     },
-    save : fuction () {
+    save : function () {
         var data = {
             title: $('#title').val(),
             author: $('#author').val(),
@@ -22,9 +24,9 @@ var main = {
             type: 'POST',
             url: '/api/v1/posts',
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
+            contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
-        }).done(function () {
+        }).done(function() {
             alert('글이 등록되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
@@ -43,9 +45,9 @@ var main = {
             type: 'PUT',
             url: '/api/v1/posts/'+id,
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
+            contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
-        }).done(function () {
+        }).done(function() {
             alert('글이 수정되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
@@ -59,15 +61,15 @@ var main = {
             type: 'DELETE',
             url: '/api/v1/posts/'+id,
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function () {
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
             alert('글이 삭제되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     }
+
 };
 
 main.init();
